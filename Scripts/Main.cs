6ec83aadfb04aac24d4f76da5a468c455e3af777
LearnPRG-py <77050478+LearnPRG-py@users.noreply.github.com>
2023-01-21 15:59:57 +0530

    finished the script behaviour of game 2

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -2,19 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class Main : MonoBehaviour
 {
     public float areaInit = 10f;
     public float forceIn = 9f;
+    public float areasel;
     public Text loltext;
+    public Text area;
+    public Text FreqText;
+    public System.Random randomforce = new System.Random();
     public float a = 1;
+    public float forcereq;
+    public float randomar;
+    public float score;
+    public float tries;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        forcereq = 23f;
+        FreqText = "Force req = "+forcereq.ToString();
     }
 
     // Update is called once per frame
@@ -24,8 +34,25 @@ public class Main : MonoBehaviour
     }
     public void SetSize(float a){
         transform.localScale = new Vector3 (a/10, 5f, 1f);
+        area.text = "Area of right = "+a.ToString();
+
     }
     public void Trolltext(){
         loltext.text = "Hmm this seems hard...";
+    }
+    public void Correct(){
+        randomar = (float)randomforce.Next(1,100);
+        forcereq = randomar*0.9f;
+        score+=1;
+        tries+=1;
+    }
+    public void Check(){
+        if (forcereq == areasel*0.9){
+            Correct()
+        }
+        else{
+            tries+=1;
+        }
+
     }
 }
