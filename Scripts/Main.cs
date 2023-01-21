a95371bb54596928ec759097966e06ade00cf834
LearnPRG-py <77050478+LearnPRG-py@users.noreply.github.com>
2023-01-21 17:17:58 +0530

    huge bugfixes

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -8,18 +8,19 @@ public class Main : MonoBehaviour
 {
     public float areaInit = 10f;
     public float forceIn = 9f;
-    public float areasel;
+    public int areasel;
     public Text loltext;
     public Text area;
     public Text FreqText;
     public System.Random randomforce = new System.Random();
     public float a = 1;
-    public float forcereq = 23f;
-    public float randomar;
+    public float forcereq = 18f;
+    public int randomar = 20;
     public float score;
     public float tries;
     public Text scoretxt;
     public Text triestxt;
+    public bool click;
 
 
     // Start is called before the first frame update
@@ -31,31 +32,34 @@ public class Main : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        scoretxt.text = "Score: " + score.ToString();
-        triestxt.text = "Tries: " + tries.ToString();
+        
     }
-    public void SetSize(float a){
-        transform.localScale = new Vector3 (a/10, 5f, 1f);
+    public void SetSize(string a){
+        transform.localScale = new Vector3 (float.Parse(a)/10, 5f, 1f);
         area.text = "Area of right = "+a.ToString();
+        areasel = (int)(float.Parse(a));
+        if (click){
+            if (randomar == areasel){
+            randomar = randomforce.Next(1,100);
+            forcereq = ((float)randomar)*0.9f;
+            FreqText.text = "Force req = "+forcereq.ToString();
+            score+=1;
+            tries+=1;
+            scoretxt.text = "Score: " + score.ToString();
+            triestxt.text = "Tries: " + tries.ToString();
+        }
+        else{
+            tries+=1;
+            scoretxt.text = "Score: " + score.ToString();
+            triestxt.text = "Tries: " + tries.ToString();
+        }
+        }
 
     }
     public void Trolltext(){
         loltext.text = "Hmm this seems hard...";
     }
-    public void Correct(){
-        randomar = (float)randomforce.Next(1,100);
-        forcereq = randomar*0.9f;
-        FreqText.text = "Force req = "+forcereq.ToString();
-        score+=1;
-        tries+=1;
-    }
     public void Check(){
-        if (forcereq == areasel*0.9){
-            Correct();
-        }
-        else{
-            tries+=1;
-        }
-
+        click = true;
     }
 }
