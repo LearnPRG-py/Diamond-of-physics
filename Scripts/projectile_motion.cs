ea14c5a68faa938ee37c9bb2a31d9e83089b1831
LearnPRG-py <77050478+LearnPRG-py@users.noreply.github.com>
2023-01-21 11:02:50 +0530

    fixed bug on playtest 1

--- Scripts/projectile_motion.cs
+++ Scripts/projectile_motion.cs
@@ -17,7 +17,7 @@ public class projectile_motion : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Reset();
     }
 
     // Update is called once per frame
