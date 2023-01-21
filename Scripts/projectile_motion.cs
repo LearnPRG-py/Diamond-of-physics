0b4f0f9ce7919ebfc23d1c37dcc7dd8fe1cf8cf3
LearnPRG-py <77050478+LearnPRG-py@users.noreply.github.com>
2023-01-21 10:33:55 +0530

    Created the get force and get angle from inputfield function
    Rest changes made by unity, also added variables

--- Scripts/projectile_motion.cs
+++ Scripts/projectile_motion.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class projectile_motion : MonoBehaviour
 {
+    public float force;
+    public float angle;
+    public InputField forceinput;
+    public InputField angleinput;
+    public Text scoretext;
+    public Text triestext;
+    public float score;
+    public float tries;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +23,11 @@ public class projectile_motion : MonoBehaviour
     void Update()
     {
         
+    }
+    void GetForce(string a){
+        force = float.Parse(a);
+    }
+    void GetAngle(string b){
+        angle = float.Parse(b);
     }
 }
