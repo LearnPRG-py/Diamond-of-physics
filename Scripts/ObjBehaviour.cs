25979e03d0a347b2a749ecc1044fe9494819fe83
LearnPRG-py <77050478+LearnPRG-py@users.noreply.github.com>
2023-01-21 18:45:37 +0530

    worked on lens behaviour

--- Scripts/ObjBehaviour.cs
+++ Scripts/ObjBehaviour.cs
@@ -1,9 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 
 public class ObjBehaviour : MonoBehaviour
 {
+    public Text objdisttxt;
+    public float focaldist = 1;
+    public float objectdist;
+    public float imagedist;
+    public float randomobjdist = -5f;
+    public float reqimgdist = 1.25f;
+    public System.Random randomobj = new System.Random();
+    public float score;
+    public float tries;
+    public Text triesTXT;
+    public Text scoreTXT;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +28,23 @@ public class ObjBehaviour : MonoBehaviour
     {
         
     }
-    void MoveObj(float a){
+    public void MoveObj(float a){
         transform.position = new Vector3 (a-9f,0.5f,0f);
+        objdisttxt.text = "Object distance = "+(a-9f).ToString();
+        objectdist = a;
+    }
+    public void FindImgDist(){
+        imagedist = focaldist*objectdist/focaldist+objectdist;
+    }
+    public void ifCorrect(){
+        randomobjdist = (float)(randomobj.Next(0, 8)-9f);
+        reqimgdist = focaldist*randomobjdist/focaldist+randomobjdist;
+
+    }
+    public void Check(){
+        FindImgDist();
+        if (randomobjdist == objectdist){
+            ifCorrect();
+        }
     }
 }
