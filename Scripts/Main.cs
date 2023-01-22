48df3b0cc8f9584c8da145f55ec42331f28f2d89
LearnPRG-py <77050478+LearnPRG-py@users.noreply.github.com>
2023-01-22 07:28:56 +0530

    finished game 2 hint system

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -21,7 +21,12 @@ public class Main : MonoBehaviour
     public Text scoretxt;
     public Text triestxt;
     public bool click;
-
+    public float scoreadd = 1f;
+    public float hints = 0f;
+    
+    public Text hint1;
+    public Text hint2;
+    public Text hint3;
 
     // Start is called before the first frame update
     void Start()
@@ -43,7 +48,7 @@ public class Main : MonoBehaviour
             randomar = randomforce.Next(1,100);
             forcereq = ((float)randomar)*0.9f;
             FreqText.text = "Force req = "+forcereq.ToString();
-            score+=1;
+            score+=scoreadd;
             tries+=1;
             scoretxt.text = "Score: " + score.ToString();
             triestxt.text = "Tries: " + tries.ToString();
@@ -61,5 +66,21 @@ public class Main : MonoBehaviour
     }
     public void Check(){
         click = true;
+    }
+    public void Hints(){
+        hints+=1;
+        if (hints == 1f){
+            hint1.text = "Pressure is equal to force by area";
+            scoreadd=1f;
+        }
+        if (hints == 2f){
+            hint2.text = "Pressure is transmitted equally throught an ideal fluid";
+            scoreadd=0.75f;
+        }
+        if (hints == 3f){
+            hint3.text = "Area on right = Force required/(force in/area left)";
+            scoreadd=0.5f;
+        }
+
     }
 }
