674b4476999a12e5866ed07995da349fa050f828
Sreshty-Beshty <126054700+Sreshty-Beshty@users.noreply.github.com>
2023-03-08 14:28:48 +0530

    Add files via upload

--- Quark Learning/main.js
+++ Quark Learning/main.js
@@ -1,1 +1,11 @@
+let situation1OpenSearch = document.getElementsByClassName("search")[0];
 
+function openSearch() {
+  if (situation1OpenSearch.style.display == "none") {
+    document.getElementsByClassName("search")[0].style.display = "inline-block";
+    document.getElementsByClassName("search")[1].style.display = "inline-block";
+  } else {
+    document.getElementsByClassName("search")[0].style.display = "none";
+    document.getElementsByClassName("search")[1].style.display = "none";
+  }
+}
